8ddd8a9946e5d26e3f13d0700f141999b434107d
Z Holka <z@holka.eu>
2025-05-02 23:10:26 +0100

    adj flaps

--- 3d.js
+++ 3d.js
@@ -111,7 +111,7 @@ function loadFlapSTLModel(isLeft = true) {
             flapRight = mesh;
         }
 
-        mesh.position.x = isLeft ? -167 : 167;
+        mesh.position.x = isLeft ? -95 : 95;
         if(isLeft){
             mesh.rotation.z = -Math.PI;
         }
